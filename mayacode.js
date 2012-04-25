d76f63d1e441e6116714fa9885de2409dbd73f8f
Daniel Wood <dan@developmentseed.org>
2012-04-25 16:58:34 -0400

    added layer arrow, new layerswitching, js remove command for legends

--- mayacode.js
+++ mayacode.js
@@ -8,15 +8,17 @@ var legend;
 
 function buildMap(layer) {
 	wax.tilejson(url + layer + '.jsonp', function(tilejson) {
-	  var m = new MM.Map('mymap', new wax.mm.connector(tilejson));
+	var m = new MM.Map('mymap', new wax.mm.connector(tilejson));
+
+	$('.wax-legend, .wax-tooltip').remove();
+
 	interaction = wax.mm.interaction()
 	  .map(m)
 	  .tilejson(tilejson)
 	  .on(wax.tooltip().animate(true).parent(m.parent).events());
 	 wax.mm.zoomer(m).appendTo(m.parent);
 	 m.setCenterZoom({ lat: 38.901, lon: -77.078 }, 12);
-	
-	$('.wax-legend').remove();
+
 	wax.mm.legend(m, tilejson).appendTo(m.parent);
 	});
 }
@@ -28,7 +30,12 @@ $(function() {
       e.preventDefault();
       $('ul.layerswitcher a').removeClass('active');
       $(this).addClass('active');
-      layer = this.id;
+      layer = $(this).attr('data-layer');
+
+      // To change the height of the bar in the sidebar we assign 
+      // and id here and change it in the css
+      var borderBar = $(this).attr('data-name');
+      $('.border-bar').attr('id', borderBar);
       buildMap(layer);
 	});
 });
