9c8484fe3ade272f712f33353309d1c7b6e6497b
Tom MacWright <tom@macwright.org>
2012-05-25 14:36:34 -0400

    Don't rebuild map

--- app.js
+++ app.js
@@ -5,22 +5,30 @@ var legend;
 
 function buildMap(layer) {
 	wax.tilejson(url + layer + '.jsonp', function(tilejson) {
-	var m = new MM.Map('mymap', new wax.mm.connector(tilejson));
+    if (!m) {
+      m = new MM.Map('mymap', new wax.mm.connector(tilejson));
 
-	$('.wax-legend').remove(); //the legend stacks with the build so needs to be removed first.
-	$('a.zoomer').hide(); //i remove the zoomer because it is transparent, and stacks
-  
+      interaction = wax.mm.interaction()
+        .map(m)
+        .tilejson(tilejson)
+        .on(wax.tooltip().animate(true).parent(m.parent).events());
 
-	interaction = wax.mm.interaction()
-	  .map(m)
-	  .tilejson(tilejson)
-	  .on(wax.tooltip().animate(true).parent(m.parent).events());
-	 wax.mm.zoomer(m).appendTo(m.parent);
-	 m.setCenterZoom({ lat: 38.901, lon: -77.078 }, 12);
-	wax.mm.legend(m, tilejson).appendTo(m.parent);
+      wax.mm.zoomer(m).appendTo(m.parent);
+
+      m.setCenterZoom({ lat: 38.901, lon: -77.078 }, 12);
+
+      legend = wax.mm.legend(m, tilejson).appendTo(m.parent);
+      attribution = wax.mm.legend(m, tilejson).appendTo(m.parent);
+
+    } else {
+      m.setLayerAt(0, new wax.mm.connector(tilejson));
+      interaction.tilejson(tilejson);
+      legend.content(tilejson.legend || '');
+      attribution.content(tilejson.attribution || '');
+    }
 	});
-
 }
+
 //could also be $(function() {
 $(document).ready(function() { 
 	buildMap(layer);
